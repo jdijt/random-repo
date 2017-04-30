e4f1cc0021ebbdd82a37982e20fed15e082d2eb4
Jasper Dijt <jasper@jasperdijt.eu>
2017-04-30 10:34:08 +0200

    Routing, form, basic frontend wiring.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,36 @@
 package controllers
 
-import play.api._
+import javax.inject.Inject
+
+import play.api.data.Forms._
+import play.api.data._
+import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc._
 
-class Application extends Controller {
+case class QueryData(query: String)
 
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+class Application @Inject()(val messagesApi: MessagesApi) extends Controller with I18nSupport {
+
+  val queryForm = Form(
+    mapping("query" -> text)(QueryData.apply)(QueryData.unapply)
+  )
+
+  def index = Action { implicit request =>
+    Ok(views.html.index(queryForm))
+  }
+
+  def queryPost = Action(parse.form(queryForm)) { implicit request =>
+    Redirect(routes.Application.queryGet(request.body.query))
+  }
+
+  def queryGet(query: String) = Action {
+    def filledForm = queryForm.fill(QueryData(query))
+
+    Ok(views.html.queryResult(filledForm, Nil))
+  }
+
+  def dashboard = Action {
+    Ok(views.html.dashboard())
   }
 
 }
